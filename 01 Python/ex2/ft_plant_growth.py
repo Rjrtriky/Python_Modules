3a68d77b72bce4747b9c30b5f839f176d3a95fa8
Rosa Juarez <rjuarez-@student.42madrid.com>
2026-04-23 23:58:48 +0200

    Add files via upload

--- 01 Python/ex2/ft_plant_growth.py
+++ 01 Python/ex2/ft_plant_growth.py
@@ -6,7 +6,7 @@
 #    By: rjuarez- <rjuarez-@student.42madrid.com>   +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/04/23 22:09:35 by rjuarez-          #+#    #+#              #
-#    Updated: 2026/04/23 22:33:20 by rjuarez-         ###   ########.fr        #
+#    Updated: 2026/04/23 23:47:10 by rjuarez-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,13 +18,13 @@ class Plant:
 		self.growth = growth
 
 	def print_plant(self) -> None:
-		print(f"{self.name}: {self.height}cm, {self.age} days old")
-      
+		print(f"{self.name}: {round(self.height, 1)}cm, {self.age} days old")
+
 	def ageing(self) -> None:
-		self.age = self.age + 1
+		self.age += 1
 
 	def growing(self) -> None:
-		self.height = self.height + self.growth
+		self.height += self.growth
 
 def main() -> None:
 	rose = Plant("Rose", 25.0, 30, 0.8)
@@ -32,14 +32,14 @@ def main() -> None:
 	
 	print("===== Garden Plant Growth ====")
 	rose.print_plant()
-	for day in range (1, 8):
+	for day in range(1, 8):
 		rose.ageing()
 		rose.growing()
 		print(f"=== Day {day} ===")
 		rose.print_plant()
 	
 	size = rose.height - size
-	print(f"Growth this week: {size}cm")
+	print(f"Growth this week: {round(size,1)}cm")
 
 if __name__ == "__main__":
 	main()
